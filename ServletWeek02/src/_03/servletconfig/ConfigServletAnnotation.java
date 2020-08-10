0d8e346a04b34b2248cd875e439728eadc00807b
Fadıl Burak Yurtsever <fadil.13urak.yurtsever@gmail.com>
2020-08-10 19:13:55 +0300

    Java Servlet JPA
    Week 02

---
+++ ServletWeek02/src/_03/servletconfig/ConfigServletAnnotation.java
@@ -0,0 +1,25 @@
+package _03.servletconfig;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/configAnnotation", initParams = @WebInitParam(name="role",value = "admin"))
+public class ConfigServletAnnotation extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String role = getServletConfig().getInitParameter("role");
+		resp.getWriter().print(role);
+		
+		//Annotation Yöntemiyle Yukarýda ki gibi xml'e yazmadan da kullana biliriz .
+		//Genel olarak sýk kullanýlan yöntem Annotation Yöntemidir. 
+	} 
+	
+}
